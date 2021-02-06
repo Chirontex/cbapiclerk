f76c01565d9ce25a49eab99970942d289b6f0df1
Dmitry Shumilin <infernozzz@yandex.ru>
2021-02-07 02:47:30 +0300

    0.3.0: add Handle::dataCrud()

--- src/Exceptions/HandleException.php
+++ src/Exceptions/HandleException.php
@@ -9,6 +9,16 @@ use Exception;
 class HandleException extends Exception
 {
 
-    
+    const BAD_ANSWER_CODE = -4;
+    const BAD_ANSWER_MESSAGE = 'Bad answer from the server was received:';
+
+    const REQUEST_FAILURE_CODE = -5;
+    const REQUEST_FAILURE_MESSAGE = 'Authentication salt request failure.';
+
+    const AUTH_FAILURE_CODE = -6;
+    const AUTH_FAILURE_MESSAGE = 'Authentication failure.';
+
+    const INVALID_ACTION_CODE = -7;
+    const INVALID_ACTION_MESSAGE = 'Invalid command.';
 
 }
--- src/Handle.php
+++ src/Handle.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * CBAPIClerk 0.0.2
+ * CBAPIClerk 0.3.0
  * Copyright (C) 2021 Dmitry Shumilin
  * 
  * MIT License
@@ -30,6 +30,11 @@ use Infernusophiuchus\CBAPIClerk\Exceptions\HandleException;
 class Handle
 {
 
+    const CREATE = 'create';
+    const READ = 'read';
+    const UPDATE = 'update';
+    const DELETE = 'delete';
+
     protected $url;
     protected $login;
     protected $key;
@@ -47,6 +52,153 @@ class Handle
             $this->$arg = $$arg;
 
         }
+
+        if (substr($this->url, -1) !== '/') $this->url .= '/';
+
+        if (substr($this->url, 0, 4) !== 'http') {
+
+            switch (substr($this->url, 0, 2)) {
+
+                case ':/':
+                    $this->url = 'http'.$this->url;
+                    break;
+
+                case '//':
+                    $this->url = 'http:'.$this->url;
+                    break;
+
+                default:
+                    $this->url = 'http://'.$this->url;
+                    break;
+
+            }
+
+        }
+
+    }
+
+    /**
+     * Send command to the server and return the answer from it.
+     * 
+     * @param string $url
+     * @param array $command
+     * 
+     * @return array
+     * 
+     * @throws Infernusophiuchus\CBAPIClerk\Exceptions\HandleException
+     */
+    protected function command(string $url, array $command) : array
+    {
+
+        if (empty($url)) throw new HandleException(
+            'The url cannot be empty.',
+            -1
+        );
+
+        $command = json_encode($command);
+
+        $ch = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $command);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-type: application/json',
+            'Content-length: '.strlen($command)
+        ]);
+
+        $answer = curl_exec($ch);
+
+        $result = json_decode(
+            $answer,
+            true
+        );
+
+        if (is_array($result)) return $result;
+        else throw new HandleException(
+            HandleException::BAD_ANSWER_MESSAGE.' "'.$answer.'"',
+            HandleException::BAD_ANSWER_CODE
+        );
+
+    }
+
+    /**
+     * Authenticate on the server.
+     * 
+     * @return string
+     * 
+     * @throws Infernusophiuchus\CBAPIClerk\Exceptions\HandleException
+     */
+    protected function auth() : string
+    {
+
+        $request = $this->command(
+            $this->url.'api/auth/request',
+            [
+                'v' => '1.0',
+                'login' => $this->login,
+                'life_time' => 60
+            ]
+        );
+
+        if ($request['code'] === 0) {
+
+            $auth = $this->command(
+                $this->url.'api/auth/auth',
+                [
+                    'v' => '1.0',
+                    'login' => $this->login,
+                    'hash' => md5($request['salt'].$this->key)
+                ]
+            );
+
+            if ($auth['code'] === 0) return $auth['access_id'];
+            else throw new HandleException(
+                HandleException::AUTH_FAILURE_MESSAGE.
+                ' Answer code: '.$auth['code'].
+                ', answer message: "'.$auth['message'].'"',
+                HandleException::AUTH_FAILURE_CODE
+            );
+
+        } else throw new HandleException(
+            HandleException::REQUEST_FAILURE_MESSAGE.
+            ' Answer code: '.$request['code'].
+            ', answer message: "'.$request['message'].'"',
+            HandleException::REQUEST_FAILURE_CODE
+        );
+
+    }
+
+    /**
+     * Perform the action with the data on the server.
+     * 
+     * @param string $action
+     * @param array $command
+     * 
+     * @return array
+     * 
+     * @throws Infernusophiuchus\CBAPIClerk\Exceptions\HandleException
+     */
+    public function dataCrud(string $action, array $command) : array
+    {
+
+        if ($action === self::CREATE ||
+            $action === self::READ ||
+            $action === self::UPDATE ||
+            $action === self::DELETE) {
+
+            $command['access_id'] = $this->auth();
+
+            return $this->command(
+                $this->url.'api/data/'.$action,
+                $command
+            );
+
+        } else throw new HandleException(
+            HandleException::INVALID_ACTION_MESSAGE,
+            HandleException::INVALID_ACTION_CODE
+        );
 
     }
 
